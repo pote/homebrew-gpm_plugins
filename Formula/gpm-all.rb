4bbfdf4d7970737d9fde20afb88053b5a4798bc3
PoTe <pote@tardis.com.uy>
2014-07-24 16:42:11 -0300

    I am idiot

--- Formula/gpm-all.rb
+++ Formula/gpm-all.rb
@@ -1,6 +1,6 @@
 require 'formula'
 
-class GpmBootstrap < Formula
+class GpmAll < Formula
   homepage 'https://github.com/pote/gpm-all'
   url      'https://github.com/pote/gpm-all/archive/v0.0.1.tar.gz'
   sha1     'ae11086086058aaa2ae011f75eb3aac78ebf4d53'
